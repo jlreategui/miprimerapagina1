8401e647541d28b544bf3aec0af5787485c4f134
jlreategui <134982600+jlreategui@users.noreply.github.com>
2023-05-29 21:45:13 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function showAlert() {
+    alert("Es solo una prueba de javaScript")
+}
+
+function sumar(a,b) {
+    let resultado = a + b;
+    alert("El resultado es :" + resultado )
+    return resultado
+}
+
+
+function handleClick() {
+    
+    alert('¡Hola! Has hecho click en el boton.');
+}
+    
+function mostrarPasos() {
+    for (let i = 1; i <= 5; ) {
+        alert("Paso " + i);
+        i++;
+    }
+}
